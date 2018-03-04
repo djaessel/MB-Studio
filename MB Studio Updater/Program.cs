2741f8082f60ea6c0fcd4263038e26adbdd444b5
Johannes Koch <hansjuergoe@gmail.com>
2018-03-04 03:02:53 +0100

    Added Project - MB Studio Updater

---
+++ MB Studio Updater/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MB_Studio_Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"F:\WORKINGAREA\Visual Studio Projects\MB_Decompiler\MB Studio\bin\x86\Release";
+            List<FileInfo> files = GetAllFiles(folderPath);
+            List<string> list = new List<string>();
+
+            foreach (FileInfo file in files)
+            {
+                string codeX = MD5Maker.CreateMD5ChecksumString(file.FullName) + "|" + file.LastWriteTimeUtc.ToFileTime() + "|." + file.FullName.Substring(folderPath.Length);
+                list.Add(codeX);
+                Console.WriteLine("Code: " + codeX + Environment.NewLine);
+            }
+
+            File.WriteAllLines(folderPath + "\\index.mbi", list);
+
+            Console.WriteLine("Root Folder: " + folderPath);
+            Console.WriteLine("Files: " + files.Count + Environment.NewLine);
+
+            Console.ReadKey();
+        }
+
+        private static List<FileInfo> GetAllFiles(string folderPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            List<FileInfo> allFiles = new List<FileInfo>();
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+
+            allFiles.AddRange(dir.GetFiles());
+
+            if (subDirs.Length != 0)
+                foreach (DirectoryInfo dirs in subDirs)
+                    allFiles.AddRange(GetAllFiles(dirs.FullName));
+
+            return allFiles;
+        }
+    }
+}
---
+++ importantLib/importantLib/MD5Maker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace MB_Studio_Updater
+{
+    public class MD5Maker
+    {
+        public static byte[] CreateMD5Checksum(string filePath)
+        {
+            byte[] md5Check;
+            try
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(filePath))
+                        md5Check = md5.ComputeHash(stream);
+                }
+            }
+            catch (Exception)
+            {
+                md5Check = new byte[0];
+            }
+            return md5Check;
+        }
+
+        public static string ConvertMD5ChecksumToString(byte[] md5Checksum)
+        {
+            return BitConverter.ToString(md5Checksum).Replace("-", string.Empty).ToLower();
+        }
+
+        public static string CreateMD5ChecksumString(string filePath)
+        {
+            return ConvertMD5ChecksumToString(CreateMD5Checksum(filePath));
+        }
+    }
+}
