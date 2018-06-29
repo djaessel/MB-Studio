3de061186f7e8dee10e2d93f464f6b31e2d5a0d6
Johannes Koch <hansjuergoe@gmail.com>
2018-06-30 00:25:04 +0200

    removed InfoPageManager from MB Studio Code (hardcoded version)

--- MB_Decompiler_Library/Properties/AssemblyInfo.cs
+++ MB_Decompiler_Library/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
@@ -17,10 +15,10 @@ using System.Runtime.InteropServices;
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
 // COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("fc643914-80e5-42ff-806b-c23e37dd9ec5")]
+//[assembly: Guid("fc643914-80e5-42ff-806b-c23e37dd9ec5")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
