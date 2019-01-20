f87e766dd7abec6ab6053e46e73440f94355586d
Johannes Koch <hansjuergoe@gmail.com>
2019-01-20 16:36:00 +0100

    changed other version infos

--- MB_Studio_Library/Properties/AssemblyInfo.cs
+++ MB_Studio_Library/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
